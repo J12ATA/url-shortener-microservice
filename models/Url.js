9e2bcd4a0c7a15656a7a88610afc1336db7ad278
Glitch (fcc-projects-url-shortener-microservice) <none>
2019-03-14 12:27:39 +0000

    🎲🔔 Updated with Glitch

--- models/Url.js
+++ models/Url.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const UrlSchema = new Schema({ 
   _id: { type: String, default: mongoose.Types.ObjectId().toString().slice(0, 9) },
-  url: { type: String }
+  url: { type: String, required: true }
 });
 const Url = mongoose.model('Url', UrlSchema);
 
