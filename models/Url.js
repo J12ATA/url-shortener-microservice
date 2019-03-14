687e07aaace2cb55e61120e82961ef481d91f787
Glitch (fcc-projects-url-shortener-microservice) <none>
2019-03-14 12:27:12 +0000

    👫🎟 Updated with Glitch

--- models/Url.js
+++ models/Url.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const UrlSchema = new Schema({ url: { type: String }});
+const UrlSchema = new Schema({ 
+  _id: { type: String, default: mongoose.Types.ObjectId().toString().slice(0, 9) },
+  url: { type: String }
+});
 const Url = mongoose.model('Url', UrlSchema);
 
 module.exports.Url = Url;
--- server.js
+++ server.js
@@ -42,10 +42,10 @@ app.post('/api/shorturl/new', (req, res, next) => {
   if (!userProvidedUrl.match(urlValidator)) res.json({error: 'Invalid URL'});
   Url.findOne({ url: userProvidedUrl }, (err, doc) => { // does url exist
     if (err) next(err);
-    if (doc) res.status(201).json({url: doc.url, shortcut: doc._id.toString().slice(0, 9)}); // output doc
+    if (doc) res.status(201).json({url: doc.url, shortcut: doc._id}); // output doc
     shortUrl.save((err, doc) => { // create new doc
       if (err) next(err);
-      res.status(201).json({url: doc.url, shortcut: doc._id.toString().slice(0, 9)}); // output new doc as json
+      res.status(201).json({url: doc.url, shortcut: doc._id}); // output new doc as json
     });
   });
 });
